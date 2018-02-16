f5976adf1197741f86fc8f1b01307cc6d0105ede
eigen field <idiotzavant@gmail.com>
2018-02-17 03:22:11 +0800

    add login logic (ie, protect /content from being accessed without first login)

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.secret_key = 'secretkey'
 Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
--- app/routes.py
+++ app/routes.py
@@ -5,9 +5,41 @@ from flask_login import \
     logout_user,\
     login_required
 from flask import \
-    render_template
+    render_template,\
+    request,\
+    redirect,\
+    url_for,\
+    session
+
+ACCESS_CONTROL = {
+    'johnny':'password1',
+    'mary':'password2',
+}
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    user = session.get('xxx',None)
+    return render_template('index.html',user=user)
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('xxx')
+        password = request.form.get('yyy')
+        if ACCESS_CONTROL.get(user,None) and password == ACCESS_CONTROL[user]:
+            session['xxx'] = user
+            return redirect(url_for('content'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('xxx')
+    return redirect(url_for('index'))
+
+@app.route('/content')
+def content():
+    user = session.get('xxx',None)
+    if user:
+        return render_template('content.html',user=user)
+    return redirect(url_for('index'))
