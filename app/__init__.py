ee577b92ed6a4d53e46dbd0f1b2d724ee0551f3e
eigen field <idiotzavant@gmail.com>
2018-02-17 11:33:12 +0800

    add redis to disallow multiple login for a user

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_redis import FlaskRedis
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = 'secretkey'
+app.config['REDIS_URL'] = "redis://localhost:6379/0"
+redis_store = FlaskRedis(app)
 Bootstrap(app)
 login = LoginManager(app)
 login.login_view = 'login'
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from app import app
+import datetime
+from app import app,redis_store
 from flask_login import \
     current_user, \
     login_user, \
@@ -19,27 +20,54 @@ ACCESS_CONTROL = {
 @app.route('/')
 @app.route('/index')
 def index():
-    user = session.get('xxx',None)
-    return render_template('index.html',user=user)
+    user = session.get('loginusername',None)
+    if user and redis_store.exists('loginusername:' + user):
+        return render_template('index.html',user=user,isactive=True)
+    return render_template('index.html',user=user,isactive=False)
 
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        user = request.form.get('xxx')
-        password = request.form.get('yyy')
+        user = request.form.get('loginusername')
+        password = request.form.get('loginpassword')
         if ACCESS_CONTROL.get(user,None) and password == ACCESS_CONTROL[user]:
-            session['xxx'] = user
-            return redirect(url_for('content'))
-    return render_template('login.html')
+            if not redis_store.exists('loginusername:' + user):
+                session['loginusername'] = user
+                redis_store.setex('loginusername:' + user,60,datetime.datetime.now())
+                return redirect(url_for('content'))
+    if 'loginusername' not in session:
+        return render_template('login.html')
+    return redirect(url_for('index'))
 
 @app.route('/logout')
 def logout():
-    session.pop('xxx')
+    if 'loginusername' in session:
+        redis_store.delete('loginusername:' + session['loginusername'])
+        session.pop('loginusername')
     return redirect(url_for('index'))
 
-@app.route('/content')
+@app.route('/content',methods=['GET','POST'])
 def content():
-    user = session.get('xxx',None)
-    if user:
-        return render_template('content.html',user=user)
+    if request.method == 'POST':
+        var1 = request.form.get('conf')
+        if var1 == 'running':
+            print("PARAM = " + var1)
+    user = session.get('loginusername',None)
+    if user and redis_store.exists('loginusername:' + user):
+        return render_template('content.html',user=user,isactive=True)
     return redirect(url_for('index'))
+
+@app.route('/update',methods=['GET','POST'])
+def update():
+    if request.method == 'POST':
+        config = request.args.get('conf')
+        print("config = " + config)
+        if config:
+            print("applyto " + config)
+    return redirect(url_for('content'))
+
+@app.before_request
+def before_request():
+    user = session.get('loginusername',None)
+    if user and not redis_store.exists('loginusername:' + user):
+        session.pop('loginusername')
